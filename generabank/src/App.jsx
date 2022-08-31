79432284bdcd0f8e15ab84a1fed1789d53fc17e8
Mafeushns <matheusnog001@gmail.com>
2022-08-31 15:04:22 -0300

    atualização de página de cadastro

--- generabank/src/App.jsx
+++ generabank/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 //Components
 import NavBar from './components/NavBar/NavBar';
