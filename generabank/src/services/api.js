d74df14db3b87bdb379dfb0d1efac912a130d3c2
Mafeushns <matheusnog001@gmail.com>
2022-09-08 10:27:37 -0300

    atualização

--- generabank/src/services/api.js
+++ generabank/src/services/api.js
@@ -1,36 +1,21 @@
 import axios from "axios";
 
 export const api = axios.create({
-    headers:{
-                     'x-apikey': '59a7ad19f5a9fa0808f11931',
-                     'Access-Control-Allow-Origin' : '*',
-                     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-                 },
-    baseURL: "https://google.com",
-        
-    
-   // https://5cca-179-108-104-153.sa.ngrok.io/api/form
+  baseURL: "https://4e25-179-108-104-153.sa.ngrok.io/oauth/token",
+  headers:{ 'Access-Control-Allow-Origin' : '*', },
+  // /api/form
 });
 
-
-/*export const createSession = async (email, password) => {
-    return api.get("",{email, password});
-}*/
-// const createSession = () => {
-//     return (
-//       axios.post("https://5cca-179-108-104-153.sa.ngrok.io/oauth/token", {
-//         headers:{
-//             'x-apikey': '59a7ad19f5a9fa0808f11931',
-//             'Access-Control-Allow-Origin' : '*',
-//             'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-//         }
-//       }
-//       ).then ((response) => {console.log(response.data)})
-//     )
-//   }
-  
-
-
   export const createSession = async (email, password) => {
-    return api.get("",{email, password}, 
+    return api.post("",{email, password}, 
    );}
+
+
+
+// const handleSubmit = data => 
+  // axios.post("https://4e25-179-108-104-153.sa.ngrok.io/oauth/token", 
+  // {
+  //   email: data.email,
+  //   password: data.password
+  // }
+  // ).then ((response) => {console.log(response.data)})
