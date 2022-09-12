22cd4c087c184feaa0ac217c41f03284939d4f41
Mafeushns <matheusnog001@gmail.com>
2022-09-12 13:41:42 -0300

    atualização

--- generabank/src/pages/Transfer/Transfer.jsx
+++ generabank/src/pages/Transfer/Transfer.jsx
@@ -1,8 +1,18 @@
-
+import styles from "./Transfer.module.css"
+import CurrencyInput from 'react-currency-masked-input'
 
 const Transfer = () => {
   return (
-    <div>Transfer</div>
+    <div className={styles.back}>
+      <div className={styles.card}>
+        <h1>Saldo da conta</h1>
+        <h2>R$ 11.000.000,00</h2>
+        <label>
+          Valor a tranferir 
+          <CurrencyInput name="myInput" type="text" required />
+        </label>
+      </div>
+    </div>
   )
 }
 
